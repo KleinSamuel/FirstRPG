4c8089f8e8582650fda7e96f9a7cafb53a32e5fc
sam <sam@sam-laptop>
2017-05-16 17:42:19 +0200

    class handling tile spritesheets

---
+++ MyFirstRPG/src/model/TileSet.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class TileSet {
+
+	public static final int TILEWIDTH = 64, TILEHEIGHT = 64;
+
+	private BufferedImage[] tiles;
+
+	public TileSet(BufferedImage bimg, int sizeX, int sizeY, int space) {
+		tiles = new BufferedImage[sizeX * sizeY];
+		
+		int i = 0;
+		for (int y = 0; y < sizeY; y++) {
+			for (int x = 0; x < sizeX; x++) {
+				tiles[i++] = bimg.getSubimage(x * (TILEWIDTH + 3), y * (TILEHEIGHT + 3), TILEWIDTH, TILEHEIGHT);
+			}
+		}
+	}
+	
+	public TileSet(String path, int sizeX, int sizeY, int space) {
+		
+		tiles = new BufferedImage[sizeX * sizeY];
+		BufferedImage tileSet;
+		
+		try {
+			tileSet = ImageIO.read(new File(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		int i = 0;
+		for (int y = 0; y < sizeY; y++) {
+			for (int x = 0; x < sizeX; x++) {
+				tiles[i++] = tileSet.getSubimage(x * (TILEWIDTH + 3), y * (TILEHEIGHT + 3), TILEWIDTH, TILEHEIGHT);
+			}
+		}
+	}
+
+	public void renderTile(Graphics g, int tileNum, int x, int y) {
+		g.drawImage(tiles[tileNum], x, y, TILEWIDTH, TILEHEIGHT, null);
+	}
+}
