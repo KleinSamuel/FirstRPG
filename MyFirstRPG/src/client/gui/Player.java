4367c3121deb36671346170a6d40839e26621935
sam <sam@sam-laptop>
2017-05-17 21:43:53 +0200

    updated player

--- MyFirstRPG/src/client/gui/Player.java
+++ MyFirstRPG/src/client/gui/Player.java
@@ -3,11 +3,17 @@ package client.gui;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import client.UserContent;
+import util.FilePathFactory;
+
 public class Player extends Creature {
 	
 	public static final int DEFAULT_HEALTH = 100;
 	public static final int DEFAULT_MANA = 100;
 	public static final int DEFAULT_SPEED = 2;
+	public static final int DEFAULT_LEVEL = 1;
+	public static final int DEFAUL_BAG_SIZE = 10;
+	
 	public static final int MARGIN_HORIZ = 14;
 	public static final int MARGIN_VERT = 2;
 	
@@ -16,11 +22,19 @@ public class Player extends Creature {
 	private Game game;
 	
 	private int id;
+	
+	UserContent content;
 
 	public Player(Game game, Level level, int x, int y, SpriteSheet playerSprite) {
 		super("Player", level, playerSprite, x, y, Entity.DEFAULT_WIDTH, Entity.DEFAULT_HEIGHT, Player.DEFAULT_HEALTH, Player.DEFAULT_SPEED);
 		this.game = game;
 		this.mana = DEFAULT_MANA;
+		
+		loadContent();
+	}
+	
+	private void loadContent() {
+		this.content = UserContent.readFromFile(FilePathFactory.getPathToPlayerSavegame());
 	}
 
 	@Override
@@ -33,7 +47,7 @@ public class Player extends Creature {
 	public void render(Graphics g) {
 		g.drawImage(image, entityX - game.getGameCamera().getxOffset(), entityY - game.getGameCamera().getyOffset(), width, height, null);
 		String name = "Player "+id;
-		drawName(g, game, name, Color.WHITE);
+		drawName(g, game, name, Color.WHITE, content.level);
 	}
 
 	public int getId() {
