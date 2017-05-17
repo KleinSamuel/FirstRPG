b0be50d998dd858f15975ddb63805478a85b76c0
sam <sam@sam-laptop>
2017-05-17 18:29:11 +0200

    added path object to handle path to walk for player

---
+++ MyFirstRPG/src/client/gui/Path.java
@@ -0,0 +1,14 @@
+package client.gui;
+
+import java.awt.Point;
+import java.util.LinkedList;
+
+public class Path {
+
+	public LinkedList<Point> pathPoints;
+	
+	public Path() {
+		this.pathPoints = new LinkedList<>();
+	}
+	
+}
