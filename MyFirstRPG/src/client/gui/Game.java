0229792dc4641de9df49a6632d4d040dd2026526
sam <sam@sam-laptop>
2017-05-17 18:30:02 +0200

    consecutive updated in main game class

--- MyFirstRPG/src/client/gui/Game.java
+++ MyFirstRPG/src/client/gui/Game.java
@@ -3,30 +3,38 @@ package client.gui;
 import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.Point;
-import java.awt.color.CMMException;
 import java.awt.image.BufferStrategy;
 import java.util.Arrays;
 import java.util.HashSet;
 
 import debug.DebugMessageFactory;
 import model.ClientConnectionEstablisher;
+import util.Utils;
 
 public class Game implements Runnable {
 
 	public static final int FPS = 60;
 	public static final long maxLoopTime = 1000 / FPS;
-	public static final int SCREEN_WIDTH = 640;
-	public static final int SCREEN_HEIGHT = 640;
+	public static final long maxRefreshTime = 60;
+	public static final int SCREEN_WIDTH = 960;
+	public static final int SCREEN_HEIGHT = 540;
 
 	public boolean running = true;
 
-	private ClientConnectionEstablisher serverConnection;
+	ClientConnectionEstablisher serverConnection;
+	
+	private HashSet<OtherPlayer> otherPlayers;
 
 	Screen screen;
 	Level level;
 	Player player;
+	TileMarker tileMarker;
+	
 	KeyManager keyManager;
+	ClickManager clickManager;
 	private Camera camera;
+	SpriteSheet playerSprite;
+	HUD hud;
 
 	BufferStrategy bufferStrategy;
 	Graphics graphics;
@@ -60,6 +68,9 @@ public class Game implements Runnable {
 		keyManager = new KeyManager();
 		screen.getCanvas().addKeyListener(keyManager);
 		
+		clickManager = new ClickManager(this);
+		screen.getCanvas().addMouseListener(clickManager);
+		
 		TileSet[] tileSet = new TileSet[1];
 		@SuppressWarnings({ "rawtypes", "unchecked" })
 		HashSet hs = new HashSet(Arrays.asList(0, 1, 2, 12, 14, 24, 25, 26));
@@ -67,10 +78,23 @@ public class Game implements Runnable {
 		
 		level = new Level(this, serverConnection.map, tileSet, true);
 		
-		SpriteSheet playerSprite = new SpriteSheet(serverConnection.playerSheet, 3, 4, 64, 64);
+		playerSprite = new SpriteSheet(serverConnection.playerSheet, 3, 4, 64, 64);
 		player = new Player(this, level, 320, 320, playerSprite);
+		player.setId(serverConnection.id);
+		
+		Utils.setFontForPlayerName(50);
+		
+		hud = new HUD(this, player);
+		
+		tileMarker = new TileMarker(this, serverConnection.tileMarkerImage, 0, 0);
 		
 		camera = new Camera(level.getSizeX(), level.getSizeY());
+		
+		/* init other players */
+		otherPlayers = new HashSet<>();
+		
+		long oldRefreshOthers = System.currentTimeMillis();
+		long refreshOthers = System.currentTimeMillis();
 
 		while (running) {
 
@@ -82,7 +106,13 @@ public class Game implements Runnable {
 			if (timestamp - oldTimestamp > maxLoopTime) {
 				continue;
 			}
-
+			
+			refreshOthers = System.currentTimeMillis();
+			if(refreshOthers - oldRefreshOthers > maxRefreshTime) {
+				updateOtherPlayers();
+				oldRefreshOthers = System.currentTimeMillis();
+			}
+			
 			render();
 
 			timestamp = System.currentTimeMillis();
@@ -99,8 +129,46 @@ public class Game implements Runnable {
 
 	public void update() {
 		keyManager.update();
-		player.setMove(getInput());
+		
+//		player.setMove(getInput());
+		player.setMove(player.walkOnPath());
+		
 		player.update();
+		tileMarker.update();
+		
+		hud.update();
+	}
+	
+	/* TODO: get new player position from server */
+	public void updateOtherPlayers() {
+		
+		String result = serverConnection.downloadPlayerData();
+		
+		otherPlayers.clear();
+		
+		String[] resArray = result.split(";");
+		
+		for (int i = 0; i < resArray.length; i++) {
+			String[] data = resArray[i].substring(1, resArray[i].length()-1).split(",");
+			
+			if(Integer.parseInt(data[0]) == player.getId()) {
+				continue;
+			}
+			
+			OtherPlayer op = new OtherPlayer(this, ""+Integer.parseInt(data[0]), level, playerSprite, Integer.parseInt(data[1]), Integer.parseInt(data[2]));
+			op.xMove = Integer.parseInt(data[3]);
+			op.yMove = Integer.parseInt(data[4]);
+			op.xPos = Integer.parseInt(data[5]);
+			otherPlayers.add(op);
+		}
+		
+	}
+	
+	public void renderOtherPlayers() {
+		for(OtherPlayer op : otherPlayers) {
+			op.setCurrentImage(op.xMove, op.yMove, op.xPos);
+			op.render(graphics);
+		}
 	}
 
 	public void render() {
@@ -116,8 +184,17 @@ public class Game implements Runnable {
 		graphics = bufferStrategy.getDrawGraphics();
 		graphics.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
+		
 		level.renderMap(graphics);
+		
+		tileMarker.render(graphics);
+
+		renderOtherPlayers();
+
 		player.render(graphics);
+		
+		hud.render(graphics);
+		
 
 		bufferStrategy.show();
 		graphics.dispose();
