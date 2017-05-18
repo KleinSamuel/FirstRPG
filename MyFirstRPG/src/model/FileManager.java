4276e83bbb1cb574760657fd3a38b730bd308356
sam <sam@sam-laptop>
2017-05-18 03:33:08 +0200

    added filemanager to handle asset files such as images, textures or config files

---
+++ MyFirstRPG/src/model/FileManager.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.awt.image.BufferedImage;
+
+public class FileManager {
+
+	public String map;
+	public BufferedImage tileset;
+	public BufferedImage playerSheet;
+	public BufferedImage tileMarkerImage;
+	public BufferedImage hudMenuImage;
+	
+}
