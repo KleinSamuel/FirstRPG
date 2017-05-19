2ccbaea1f8402c8cb4db8f4bc90d2c7faeb2e2af
sam <sam@sam-hackstation>
2017-05-19 15:59:09 +0200

    added item images to bag

--- MyFirstRPG/src/client/gui/HUD_Bag.java
+++ MyFirstRPG/src/client/gui/HUD_Bag.java
@@ -5,13 +5,17 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Map.Entry;
+
+import model.items.ItemFactory;
 
 public class HUD_Bag {
 
 	public static boolean DRAW_BAG = false;
 	public static final int ITEM_PADDING = 15;
-	public static final int ITEM_WIDTH = 64;
-	public static final int ITEM_HEIGHT = 64;
+	public static final int ITEM_WIDTH = 42;
+	public static final int ITEM_HEIGHT = 42;
+	public static final int INTERNAL_PADDING = 20;
 	
 	private Game game;
 	private HUD_Rectangle background;
@@ -52,17 +56,34 @@ public class HUD_Bag {
 	}
 	
 	public void render(Graphics g) {
+		
+		/* render bag background */
 		g.setColor(background.getColor());
 		g.fillRoundRect(background.getX(), background.getY(), background.getWidth(), background.getHeight(), 50, 50);
 		
+		/* render bag string */
 		g.setFont(new Font(Font.MONOSPACED, Font.BOLD, 50));
 		g.setColor(Color.WHITE);
 		g.drawString("BAG", Game.SCREEN_WIDTH/2-50, 50);
 		
+		/* render bag background tiles */
 		g.setColor(bag_content_color);
 		for(Point p : bag_content_positions) {
-			g.fillRoundRect((int)p.getX(), (int)p.getY(), ITEM_WIDTH, ITEM_HEIGHT, 20, 20);
+			g.fillRoundRect((int)p.getX(), (int)p.getY(), ITEM_WIDTH+INTERNAL_PADDING, ITEM_HEIGHT+INTERNAL_PADDING, 20, 20);
 		}
+		
+		g.setColor(Color.BLACK);
+		g.setFont(new Font(Font.MONOSPACED, Font.BOLD, 25));
+		/* render bag content */
+		int pointIndex = 0;
+		for(Entry<Integer, Integer> entry : game.player.content.getBag().entrySet()) {
+			int x = bag_content_positions.get(pointIndex).x;
+			int y = bag_content_positions.get(pointIndex).y;
+			g.drawImage(ItemFactory.getImageForItemId(game.serverConnection.fileManager, entry.getKey()), (INTERNAL_PADDING/2)+x, (INTERNAL_PADDING/2)+y, ITEM_WIDTH, ITEM_HEIGHT, null);
+			g.drawString(""+entry.getValue(), x+ITEM_WIDTH, y+ITEM_HEIGHT+INTERNAL_PADDING-3);
+			pointIndex++;
+		}
+		
 	}
 	
 }
